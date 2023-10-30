994e12672cf535d27874ebf96ae691894dc8f610
dep <117683899+dep689@users.noreply.github.com>
2023-10-31 02:49:11 +0900

    Update main.js

--- icgxr/main.js
+++ icgxr/main.js
@@ -37,7 +37,7 @@ function initGraph() {
     graph.vertices[i] = new THREE.Mesh(vertexGeometry, vertexMaterial);
     graph.vertices[i].position.x = 0.3 * Math.cos(2 * i * Math.PI / graph.order);
     graph.vertices[i].position.y = 1.6 + 0.3 * Math.sin(2 * i * Math.PI / graph.order);
-    graph.vertices[i].position.z = -1;
+    graph.vertices[i].position.z = -0.5;
   }
 
   // 辺
